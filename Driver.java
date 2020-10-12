f52f9c3757a7f4b5f0f6de7ae2f995e63de1a97e
lennonbe <d.lennonbeato@lancaster.ac.uk>
2020-10-12 15:31:49 +0100

    Commit no.2 with a slightly better javadoc of the classes ive created

--- Driver.java
+++ Driver.java
@@ -1,3 +1,7 @@
+/**
+ * This class provides the runnable instance of the MySolarSystem class.
+ * This the class which after compilations is perfromed. So essentially java Run.
+ */
 public class Driver
 {
     public static void main(String[] args)
--- doc/type-search-index.js
+++ doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"SolarSystem"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Driver"},{"p":"<Unnamed>","l":"MySolarSystem"},{"p":"<Unnamed>","l":"SolarSystem"}]
